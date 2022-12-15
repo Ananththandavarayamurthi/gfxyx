dc15f0f4704e71a58ac20f31ab3ef4a872268996
Ananththandavarayamurthi <110927979+Ananththandavarayamurthi@users.noreply.github.com>
2022-12-15 21:38:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const BREED_URL = "https://dog.ceo/api/breeds/list/all";
+const select = document.querySelector('.breeds-selection');
+// displaying the breed list to the select tag
+fetch(BREED_URL)
+    .then (function (response){
+        return response.json();
+    })
+    .then (function (data){
+        const breedObject = data.message;
+        const breedArray = Object.keys(breedObject);
+
+        for(let i = 0; i < breedArray.length; i++){
+            const option = document.createElement('option');
+            option.value =  breedArray[i];
+            option.innerText = breedArray[i];
+            select.appendChild(option);
+        }
+    })
+
+    select.addEventListener("change", function(event){
+        // url to random image
+        let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+        // get the url
+        getDog(url);
+    });
+//fetch the url and change the src of the image
+const img = document.querySelector(".dog-img");
+const spinner = document.querySelector('.spinner');
+
+    function getDog(url){
+        spinner.classList.add("show");
+        img.classList.remove("show")
+        fetch(url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                img.src = data.message;
+                spinner.classList.remove("show");
+                img.classList.add("show");
+            })
+    }
